a823d639ae4e30cf13ff0c63ef4938954a3b838a
Simon Herout <simon@herout.eu>
2021-08-10 16:24:27 +0200

    plus qu'a faire le bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get    '/articles',          to: 'articles#index',   as: :articles
+  post   '/articles',          to: 'articles#create'
+  get    '/articles/new',      to: 'articles#new',     as: :new_article
+  get    '/articles/:id',      to: 'articles#show',    as: :article
+  delete 'articles/:id',       to: 'articles#destroy'
+  get    '/articles/:id/edit', to: 'articles#edit',    as: :edit_articles
+  patch  '/articles/:id',      to: 'articles#update'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "Destroying all pre-existing articles"
+Article.destroy_all
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:   Faker::Superhero.name,
+    content: Faker::ChuckNorris.fact
+  )
+  article.save!
+  print "."
+end
+puts ""
+puts Article.all.count
